5f91583c6c7f351259979a55552c3f7963450f68
Daniel Bridewell <48977884+daniel-bridewell@users.noreply.github.com>
2019-05-30 11:52:18 +1200

    FINISHED

--- game.js
+++ game.js
@@ -1,7 +1,53 @@
 module.exports = {
-  scoreFrame: scoreFrame
+  scoreFrame: scoreFrame,
+  scoreSpare: scoreSpare,
+  scoreStrike: scoreStrike,
+  scoreGame: scoreGame,
+  strikeLastFrame: strikeLastFrame
 }
 
-function scoreFrame() {
-  return 0
+function scoreFrame(frame) {
+  return frame[0] + frame[1]
+}
+
+function scoreSpare(frame, nextFrame) {
+  return frame[0] + frame[1] + nextFrame[0]
+}
+
+function scoreStrike(frame, nextFrame, thirdFrame) {
+  var result = 10
+  if (nextFrame[0] == 10) {
+    result += 10 + thirdFrame[0]
+  } else {
+    result += scoreFrame(nextFrame)
+  }
+  return result
+}
+
+function strikeLastFrame(frame) {
+  var total = 0
+  for (var i = 0; i < frame.length; i++) {
+    total += frame[i]
+  }
+  return total
+}
+
+function scoreGame(fullFrames) {
+  var result = 0
+  for (var i = 0; i < fullFrames.length; i++) {
+    var nextFrame = fullFrames[i + 1]
+    var thirdFrame = fullFrames[i + 2]
+    var currentFrame = fullFrames[i]
+    // console.log(fullFrames[i], scoreFrame(fullFrames[i]))
+    if (currentFrame[0] == 10 && i < 9) {
+      result += scoreStrike(currentFrame, nextFrame, thirdFrame)
+    } else if (currentFrame[0] + currentFrame[1] == 10 && i < 9) {
+      result += scoreSpare(currentFrame, nextFrame)
+    } else if (currentFrame[0] == 10 && i == 9) {
+      result += strikeLastFrame(currentFrame)
+    } else {
+      result += scoreFrame(currentFrame)
+    }
+  }
+  return result
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -6,4 +6,73 @@ test('Scores a gutterball - rookie', () => {
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
-test('')
+
+test('Score Normal frame', () => {
+  const frame = [1, 7]
+  const expected = 8
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('Score a spare', () => {
+  const frame = [6, 4]
+  const nextFrame = [3, 2]
+  const expected = 13
+  const actual = game.scoreSpare(frame, nextFrame)
+  expect(actual).toBe(expected)
+
+})
+
+test('Score a single Strike', () => {
+  const frame = [10, 0]
+  const nextFrame = [8, 1]
+  const expected = 19
+  const actual = game.scoreStrike(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('Score a double Strike', () => {
+  const frame = [10, 0]
+  const nextFrame = [10, 0]
+  const thirdFrame = [8, 1]
+  const expected = 28
+  const actual = game.scoreStrike(frame, nextFrame, thirdFrame)
+  expect(actual).toBe(expected)
+})
+
+test('score a game', () => {
+  const fullFrames = [
+    [9, 1],
+    [1, 4],
+    [3, 2],
+    [0, 6],
+    [4, 3],
+    [0, 0],
+    [3, 7],
+    [4, 5],
+    [1, 6],
+    [8, 0],
+  ]
+  const expected = 72
+  const actual = game.scoreGame(fullFrames)
+  expect(actual).toBe(expected)
+})
+
+test('score a game with strikes in last frame', () => {
+  const fullFrames = [
+    [9, 1],
+    [1, 4],
+    [3, 2],
+    [0, 6],
+    [4, 3],
+    [0, 0],
+    [3, 7],
+    [4, 5],
+    [1, 6],
+    [10, 10, 10],
+
+  ]
+  const expected = 94
+  const actual = game.scoreGame(fullFrames)
+  expect(actual).toBe(expected)
+})
