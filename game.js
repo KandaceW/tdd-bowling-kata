94d19d82b6e9002a8fab844aea862f163ff93813
Tessa <49006336+tessajane@users.noreply.github.com>
2019-05-30 10:55:25 +1200

    last commit; git push

--- game.js
+++ game.js
@@ -2,5 +2,11 @@ module.exports = {
     scoreFrame:scoreFrame
 }
 function scoreFrame(frame) {
-    return 0
+    var result = frame[0] + frame[1]
+    if (result !== 0){
+        return result
+    } else {
+        return 0
+    }
 }
+
