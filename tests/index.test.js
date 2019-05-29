43041ed2f355fcafbd3429725b0c1efee4b06fe8
john-w-aupouri <49456910+john-w-aupouri@users.noreply.github.com>
2019-05-30 11:22:25 +1200

    "Scoring gutterball frames"

---
+++ tests/index.test.js
@@ -0,0 +1,12 @@
+const game = require('../game') // this is the line to add
+
+test('test setup working', () => {
+  expect(true).toBeTruthy()
+})
+
+test('scores a gutterball frame', () => {
+  const frame = [0, 0]
+  const expected = 0
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
