5c7aa00734684104caec507d5cf90d08f450d799
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-05-30 11:04:34 +1200

    passed strike test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   scoreFrame: scoreFrame,
   scoreNormalFrame: scoreNormalFrame,
   scoreSpareFrame: scoreSpareFrame,
+  scoreSingleStrike: scoreSingleStrike,
 }
 
 function scoreFrame(frame, nextFrame) {
@@ -12,14 +13,22 @@ function scoreFrame(frame, nextFrame) {
   if ((ballOne + ballTwo) < 10)
     return scoreNormalFrame(frame)
 
-  else if ((ballOne + ballTwo) === 10)
+  else if ((ballOne + ballTwo) === 10 && ballOne !== 10)
     return scoreSpareFrame(frame, nextFrame)
+
+  else if (ballOne === 10)
+    return scoreSingleStrike(frame, nextFrame)
 }
+
 
 function scoreNormalFrame(frame) {
 
   return ballOne + ballTwo
 }
 function scoreSpareFrame(frame, nextFrame) {
-  return ballOne + ballTwo + nextFrame[1]
+  return 10 + nextFrame[0]
+}
+
+function scoreSingleStrike(frame, nextFrame) {
+  return 10 + nextFrame[0] + nextFrame[1]
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -20,8 +20,16 @@ test('scores a normal frame', () => {
 
 test('scores a spare frame', () => {
   const frame = [6,4]
-  const nextFrame = [2,2]
+  const nextFrame = [2,4]
   const expected = 12
+  const actual = game.scoreFrame(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike frame', () => {
+  const frame = [10,0]
+  const nextFrame = [6,2]
+  const expected = 18
   const actual = game.scoreFrame(frame, nextFrame)
   expect(actual).toBe(expected)
 })
