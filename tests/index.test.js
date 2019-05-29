0f5bfbbed57a1eac89dbc9b744d3d746a54fedf1
student <kiwdev1984@icloud.com>
2019-05-30 11:37:48 +1200

    First Commit

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,22 @@
+const game = require('../game') // this is the line to add
+
 test ('test setup working', () => {
   expect (true).toBeTruthy()
 })
+
+test ('scores a gutterball frame', () => {
+
+const frame = [0, 0]
+const expected = 0
+const actual = game.scoreFrame(frame)
+expect(actual).toBe(expected)
+})
+
+//module.exports = {
+
+  //game: game,
+  //scoreFrame: scoreFrame
+//}
+
+//function scoreFrame (frame){
+//}
