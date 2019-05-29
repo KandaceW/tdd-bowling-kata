cb75272c69cbd2c931d17a991771abcb70617167
Tessa <49006336+tessajane@users.noreply.github.com>
2019-05-30 11:51:59 +1200

    last commit; git push

--- game.js
+++ game.js
@@ -1,10 +1,15 @@
 module.exports = {
     scoreFrame:scoreFrame
 }
-function scoreFrame(frame) {
+function scoreFrame(frame, nextFrame) {
     var result = frame[0] + frame[1]
-    if (result !== 0){
+    if (result !== 0 && result !== 10){
         return result
+    } else if( result == 10) {
+        return result + nextFrame[0]
+    } else if (frame[0] == 10) {
+        var nextResult = nextFrame[0] + nextFrame[1]
+        return result + nextResult
     } else {
         return 0
     }
--- tests/first.test.js
+++ tests/first.test.js
@@ -17,3 +17,19 @@ test ('scores a normal frame', () => {
     const actual = game.scoreFrame(frame)
     expect(actual).toBe(expected)
 })
+
+test ('scores a spare frame', () => {
+    const frame = [3, 7]
+    const nextFrame = [1, 3]
+    const expected = 11
+    const actual = game.scoreFrame(frame, nextFrame)
+    expect(actual).toBe(expected)
+})
+
+test ('scores a strike frame', () => {
+    const frame = [10, 0]
+    const nextFrame = [8,1]
+    const expected = 19
+    const actual = game.scoreFrame(frame, nextFrame)
+    expect(actual).toBe(expected)
+})
