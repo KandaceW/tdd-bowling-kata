1a3ab2209eac67ef0e2a53bfba6c794d7d1e74d9
dan <mikewipa@gmail.com>
2019-05-30 11:55:48 +1200

    Add tests

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 module.exports = {
   scoreFrame: scoreFrame,
-  scoreSpare: scoreSpare
+  scoreSpare: scoreSpare,
+  scoreStrike: scoreStrike,
+  scoreDoubleStrike: scoreDoubleStrike,
+  isSpare: isSpare,
+  isStrike: isStrike
 }
 
 function scoreFrame(frame) {
@@ -9,4 +13,24 @@ function scoreFrame(frame) {
 
 function scoreSpare(frame, nextFrame) {
   return frame[0] + frame[1] + nextFrame[0]
+}
+
+function scoreStrike(frame, nextFrame) {
+  return scoreFrame(frame) + scoreFrame(nextFrame)
+}
+
+function scoreDoubleStrike(frame, nextFrame, thirdFrame){
+  return scoreFrame(frame) + scoreFrame(nextFrame) + thirdFrame[0]
+}
+
+function isSpare(frame){
+    if (scoreFrame(frame) === 10 && frame[0] !== 10) {
+      return true
+    } else {
+      return false
+    }
+}
+
+function isStrike(frame) {
+  return (frame[0] === 10 ? true : false)
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -26,3 +26,51 @@ test('scores a spare frame', () => {
   expect(actual).toBe(expected)
 })
 
+test('scores a spare frame with different results', () => {
+  const frame = [5, 5]
+  const nextFrame = [2, 3]
+  const expected = 12
+  const actual = game.scoreSpare(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike frame', () => {
+  const frame = [10, 0]
+  const nextFrame = [5, 2]
+  const expected = 17
+  const actual = game.scoreStrike(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike frame with different results', () => {
+  const frame = [10, 0]
+  const nextFrame = [8, 1]
+  const expected = 19
+  const actual = game.scoreStrike(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a double strike frame', () => {
+  const frame = [10, 0]
+  const nextFrame = [10, 0]
+  const thirdFrame = [1, 0]
+  const expected = 21
+  const actual = game.scoreDoubleStrike(frame, nextFrame, thirdFrame)
+  expect(actual).toBe(expected)
+})
+
+test('checks if frames is a spare', () => {
+  const frameIsSpare = [3, 7]
+  const frameNotSpare = [10, 0]
+  const expected = [true, false]
+  const actual = [game.isSpare(frameIsSpare), game.isSpare(frameNotSpare)]
+  expect(actual).toEqual(expected)
+})
+
+test('checks if frame is a strike', () => {
+  const frameIsStrike = [10, 0]
+  const frameNotStrike = [5, 5]
+  const expected = [true, false]
+  const actual = [game.isStrike(frameIsStrike), game.isStrike(frameNotStrike)]
+  expect(actual).toEqual(expected)
+})
