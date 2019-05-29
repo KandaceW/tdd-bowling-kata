01032d9d1ccea07008261f9914d8597f8a4ba4ed
Tess Meyer <49256315+tess-meyer@users.noreply.github.com>
2019-05-30 11:48:31 +1200

    passed strike test

--- index.js
+++ index.js
@@ -3,32 +3,57 @@ module.exports = {
   scoreNormalFrame: scoreNormalFrame,
   scoreSpareFrame: scoreSpareFrame,
   scoreSingleStrike: scoreSingleStrike,
+  scoreDoubleStrike: scoreDoubleStrike,
+  scoreGame: scoreGame,
 }
 
-function scoreFrame(frame, nextFrame) {
+function scoreGame(frames) {
 
+  var score = 0
+  for (var i = 0; i < frames.length; i++) {
+
+    var frame = frames[i];
+    var nextFrame = frames[i + 1];
+    var thirdFrame = frames[i + 2];
+
+    score += (scoreFrame(frames[i], nextFrame, thirdFrame))
+  }
+  return score
+}
+
+function scoreFrame(frame, nextFrame, thirdFrame) {
+  console.log(frame)
   ballOne = frame[0];
   ballTwo = frame[1];
 
-  if ((ballOne + ballTwo) < 10)
-    return scoreNormalFrame(frame)
+  if ((ballOne + ballTwo) < 10) {
+    return scoreNormalFrame(frame, nextFrame, thirdFrame)
+  }
+  else if ((ballOne + ballTwo) === 10 && ballOne !== 10) {
+    return scoreSpareFrame(frame, nextFrame, thirdFrame)
+  }
 
-  else if ((ballOne + ballTwo) === 10 && ballOne !== 10)
-    return scoreSpareFrame(frame, nextFrame)
-
-  else if (ballOne === 10)
-    return scoreSingleStrike(frame, nextFrame)
+  else if (ballOne === 10 && nextFrame[0] !== 10) {
+    return scoreSingleStrike(frame, nextFrame, thirdFrame)
+  }
+  else if (ballOne === 10 && nextFrame[0] === 10) {
+    return scoreDoubleStrike(frame, nextFrame, thirdFrame)
+  }
 }
 
 
-function scoreNormalFrame(frame) {
+function scoreNormalFrame(frame, nextFrame, thirdFrame) {
 
   return ballOne + ballTwo
 }
-function scoreSpareFrame(frame, nextFrame) {
+function scoreSpareFrame(frame, nextFrame, thirdFrame) {
   return 10 + nextFrame[0]
 }
 
-function scoreSingleStrike(frame, nextFrame) {
+function scoreSingleStrike(frame, nextFrame, thirdFrame) {
   return 10 + nextFrame[0] + nextFrame[1]
+}
+
+function scoreDoubleStrike(frame, nextFrame, thirdFrame) {
+  return 20 + thirdFrame[0]
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -19,17 +19,55 @@ test('scores a normal frame', () => {
 })
 
 test('scores a spare frame', () => {
-  const frame = [6,4]
-  const nextFrame = [2,4]
+  const frame = [6, 4]
+  const nextFrame = [2, 4]
   const expected = 12
   const actual = game.scoreFrame(frame, nextFrame)
   expect(actual).toBe(expected)
 })
 
 test('scores a single strike frame', () => {
-  const frame = [10,0]
-  const nextFrame = [6,2]
+  const frame = [10, 0]
+  const nextFrame = [6, 2]
   const expected = 18
   const actual = game.scoreFrame(frame, nextFrame)
   expect(actual).toBe(expected)
 })
+
+test('scores a double strike frame', () => {
+  const frame = [10, 0]
+  const nextFrame = [10, 0]
+  const thirdFrame = [3, 4]
+  const expected = 23
+  const actual = game.scoreFrame(frame, nextFrame, thirdFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a simple game', () => {
+  const frames = [
+    [3, 4],
+    [3, 4],
+    [3, 4],
+    [3, 4],
+    [3, 4],
+    [3, 4],
+    [3, 4],
+    [3, 4],
+    [3, 4],
+    [3, 4],]
+  const expected = 70
+  const actual = game.scoreGame(frames)
+  expect(actual).toBe(expected)
+})
+
+test('scores a game with a strike', () => {
+  const frames = [
+    [3, 4], //7
+    [10, 0], //17
+    [3, 4]] //7
+
+  const expected = 31
+  const actual = game.scoreGame(frames)
+  expect(actual).toBe(expected)
+})
+
