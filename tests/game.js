c2b6b87f9b74f3a51c0fad73db68a033dff8e8ed
Tom Doesburg <8284791+tom-doesburg@users.noreply.github.com>
2019-05-30 14:02:45 +1200

    Updated Challenge

--- tests/game.js
+++ tests/game.js
@@ -1,8 +1,30 @@
 module.exports = {
-  scoreFrame: scoreFrame
+  scoreFrame: scoreFrame,
+  scoreSpare: scoreSpare,
+  scoreStrike: scoreStrike,
+  scoreDoubleStrike: scoreDoubleStrike
 }
-
 
 function scoreFrame (frame) {
   return frame[0] + frame[1]
+  
 }
+
+function scoreSpare (frame, frame2) {
+    return frame[0] + frame[1] + frame2[0]
+}
+
+function scoreStrike (frame, frame2) {
+  return frame[0] + frame2[0] + frame2[1]
+}
+
+function scoreDoubleStrike (frame, frame2, frame3) {
+  if (frame[0] === 10 && frame2[0] === 10) {
+    return frame[0] + frame2[0] + frame3[0] + frame3[0]
+  }
+}
+
+//function spareFrame (frame, frame2) {
+//var newArr = 0
+// if frame = 10 then add frame2 function
+//  result frame + frame2[0]
--- tests/index.test.js
+++ tests/index.test.js
@@ -17,3 +17,29 @@ test('scores a normal frame', () => {
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
+
+test('scores a spare frame', () => {
+  const frame = [7, 3]
+  const frame2 = [1, 0]
+  const expected = 11
+  const actual = game.scoreSpare(frame, frame2)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike frame', () => {
+  const frame = [10, 0]
+  const frame2 = [1, 8]
+  const expected = 19
+  const actual = game.scoreStrike(frame, frame2)
+  expect(actual).toBe(expected)
+})
+
+test('scores a double strike frame', () => {
+  const frame = [10, 0]
+  const frame2 = [10, 0]
+  const frame3 = [6, 3]
+  const expected = 29
+  const actual = game.scoreDoubleStrike(frame, frame2, frame3)
+  expect(actual).toBe(expected)
+})
+
