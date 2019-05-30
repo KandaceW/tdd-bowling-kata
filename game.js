d1bfcf62b3f1b8fbab5cf354d9852b61bf738012
Tessa <49006336+tessajane@users.noreply.github.com>
2019-05-30 16:07:07 +1200

    last commit; git push

--- game.js
+++ game.js
@@ -1,17 +1,42 @@
 module.exports = {
-    scoreFrame:scoreFrame
+    scoreFrame:scoreFrame,
+    doubleStrike: doubleStrike,
+    addGame: addGame
 }
+
 function scoreFrame(frame, nextFrame) {
     var result = frame[0] + frame[1]
     if (result !== 0 && result !== 10){
         return result
-    } else if( result == 10) {
-        return result + nextFrame[0]
     } else if (frame[0] == 10) {
         var nextResult = nextFrame[0] + nextFrame[1]
+  
+        
         return result + nextResult
-    } else {
+    } else if(result == 10 && frame[0] !== 10) {
+        return result + nextFrame[0]
+    }  else {
         return 0
     }
 }
 
+function doubleStrike(frames){
+    var newArr = []
+    
+    for (var i = 0; i < frames.length; i++) {
+        var firstBall = frames[i][0]
+        var secondBall = frames[i][1]
+        var result = firstBall + secondBall
+        newArr.push(result)   
+    }
+    for(var i = 0; i < newArr.length; i++){
+        if (newArr[i] === 10 && newArr[i + 1] === 10 ){
+            var strikeResult = newArr[i] + newArr[i + 1] + newArr[i + 2]
+            return strikeResult
+        } 
+    }
+
+    function addGame(frames){
+
+    }
+}
--- tests/first.test.js
+++ tests/first.test.js
@@ -33,3 +33,19 @@ test ('scores a strike frame', () => {
     const actual = game.scoreFrame(frame, nextFrame)
     expect(actual).toBe(expected)
 })
+
+test ('score a double strike', () => {
+    const frames = [[10, 0], [10, 0], [6, 1]]
+    const expected = 27
+    const actual = game.doubleStrike(frames)
+    expect(actual).toBe(expected)
+})
+
+test ('scores a game', () => {
+    const frames = [[2, 3], [4, 5], [7, 2], [10, 0], [9, 0], [6, 3], [3, 1], [1, 1], [6, 4], [7, 3,63]]
+    const expected = 98
+    const actual = game.addGame(frames)
+    expect(actual).toBe(expected)
+})
+
+
