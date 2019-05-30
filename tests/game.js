171753ed6d19db404e6e258c0bced63e4cfbceb1
Tom Doesburg <8284791+tom-doesburg@users.noreply.github.com>
2019-05-30 15:39:32 +1200

    Updated index and game

--- tests/game.js
+++ tests/game.js
@@ -2,8 +2,25 @@ module.exports = {
   scoreFrame: scoreFrame,
   scoreSpare: scoreSpare,
   scoreStrike: scoreStrike,
-  scoreDoubleStrike: scoreDoubleStrike
+  scoreDoubleStrike: scoreDoubleStrike,
+  scoreTerribleGame: scoreTerribleGame
 }
+
+var frames = [
+  [1, 2], 
+  [6, 3], 
+  [5, 4], 
+  [1, 0], 
+  [7, 2], 
+  [1, 0], 
+  [1, 0], 
+  [5, 2], 
+  [7, 0], 
+  [4, 4]
+
+  ]
+
+  
 
 function scoreFrame (frame) {
   return frame[0] + frame[1]
@@ -20,8 +37,15 @@ function scoreStrike (frame, frame2) {
 
 function scoreDoubleStrike (frame, frame2, frame3) {
   if (frame[0] === 10 && frame2[0] === 10) {
-    return frame[0] + frame2[0] + frame3[0] + frame3[0]
+    return frame[0] + frame2[0] + frame3[0]
   }
+}
+
+function scoreTerribleGame (frames) {
+  var result = 0
+  for(var i = 0; i < frames.length; i++) {
+  result = result + scoreTerribleGame(frames[i])
+}
 }
 
 //function spareFrame (frame, frame2) {
--- tests/index.test.js
+++ tests/index.test.js
@@ -37,9 +37,23 @@ test('scores a single strike frame', () => {
 test('scores a double strike frame', () => {
   const frame = [10, 0]
   const frame2 = [10, 0]
-  const frame3 = [6, 3]
-  const expected = 29
+  const frame3 = [1, 8]
+  const expected = 21
   const actual = game.scoreDoubleStrike(frame, frame2, frame3)
   expect(actual).toBe(expected)
 })
+
+test('scores a terrible game', () => {
+  const frames = [[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],[0, 0],]
+  const expected = 0
+  const actual = game.scoreTerribleGame(frames)
+  expect(actual).toBe(expected)
+})
+
+// test('scores a great game', () => {
+//   const frames = [[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 0],[10, 10, 10],]
+//   const expected = 300
+//   const actual = game.scoreGreatGame(frames)
+//   expect(actual).toBe(expected)
+// })
 
