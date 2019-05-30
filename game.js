e24796bcf09224ad606eaa60e390a2020f6939d4
Dan van Ammers <49254249+daniel-vanammers@users.noreply.github.com>
2019-05-30 16:54:59 +1200

    complete tdd-bowling

--- game.js
+++ game.js
@@ -1,24 +1,51 @@
 module.exports = {
     scoreFrame: scoreFrame,
-    // spareFrame: spareFrame
+    spareFrame: spareFrame,
+    strikeFrame: strikeFrame,
+    scoreGame: scoreGame,
+    sumLastFrame: sumLastFrame
 }
 
-function scoreFrame (frame) {
-    return frame[0] + frame[1];
-
+function scoreFrame (firstFrame) {
+    return firstFrame[0] + firstFrame[1];
+}
+    
+function spareFrame (firstFrame, secondFrame) {
+    var result = 0
+    if (scoreFrame(firstFrame) === 10){
+    result += 10+ secondFrame[0]   
+    }
+    return result
 }
 
-// function spareFrame (frame) {
-//     var ballOne = frame[i][0];
-//     var ballTwo = frame[i][1];
-//     var nextBall = frame[i][0]
+function strikeFrame (firstFrame, secondFrame, thirdFrame) {
+    var result = 0
+    if (firstFrame[0] === 10 && secondFrame[0] === 10){
+    result += (scoreFrame(firstFrame) + scoreFrame(secondFrame) + thirdFrame[0]) 
+    } else if (firstFrame[0] === 10){
+    result += scoreFrame(secondFrame) + 10
+    }
+    return result
+}
 
-//     for (var i = 0; i < frames.length; i++){
-//         if(ballOne + ballTwo === 10) {
-//          frames[i] = ballOne + ballTwo + nextBall
-//     } else {
-//         scoreFrame(frames)
-//     }
-//   } 
-// }
+function scoreGame (frames) {
+    let result = 0
 
+    for (var i = 0; i <frames.length; i++){
+        var firstFrame = frames[i]
+        var secondFrame = frames[i+1]
+        var thirdFrame = frames[i+2]
+        // console.log(result)
+       if (firstFrame[0] === 10 && i < 9) {
+           result+=strikeFrame(firstFrame, secondFrame, thirdFrame)
+       } else if (firstFrame + secondFrame === 10 && i < 9) { result+=spareFrame (firstFrame, secondFrame) 
+       } else { result+=scoreFrame(firstFrame)}
+    }
+    return result
+}
+
+function sumLastFrame (frames) {
+    result = 0
+    lastFrame = frames[frames.length-1]
+    return lastFrame[0] + lastFrame[1] + lastFrame[2];
+}
--- tests/index.test.js
+++ tests/index.test.js
@@ -4,25 +4,62 @@ test('test setup working', () => {
     expect(true).toBeTruthy()
   })
 
-  test('scores a gutterball frame', () => {
-    const frame = [0, 0]
+  test('scores a gutterball frames', () => {
+    const firstFrame = [0, 0]
     const expected = 0
-    const actual = game.scoreFrame(frame)
+    const actual = game.scoreFrame(firstFrame)
     expect(actual).toBe(expected)
   })
   
-  test('scores a normal frame', () => {
-      const frame = [2, 3];
+  test('scores a normal frames', () => {
+      const firstFrame = [2, 3];
       const expected = 5;
-      const actual = game.scoreFrame(frame);
+      const actual = game.scoreFrame(firstFrame);
       expect(actual).toBe(expected)
   })
 
-  test('scores a spare frame', () => {
-      const frame1 = [6, 4];
-      const frame2 = [5, 3];
+  test('scores a spare frames', () => {
+      const firstFrame = [6, 4];
+      const secondFrame = [5, 3];
       const expected = 15;
-      const actual = game.scoreFrame(frame1, frame2);
+      const actual = game.spareFrame(firstFrame, secondFrame);
 
       expect(actual).toBe(expected)
 })
+
+test('scores a single strike frames', () => {
+    const firstFrame = [10, 0];
+    const secondFrame = [5, 3];
+    const expected = 18;
+    const actual = game.strikeFrame(firstFrame, secondFrame);
+
+    expect(actual).toBe(expected)
+})
+
+
+test('scores a double strike frames', () => {
+    const firstFrame = [10, 0];
+    const secondFrame = [10, 0];
+    const thirdFrame = [5, 3];
+    const expected = 25;
+    const actual = game.strikeFrame(firstFrame, secondFrame, thirdFrame);
+
+    expect(actual).toBe(expected);
+})
+
+test('scores a game', () => {
+    const frames = [[10, 0], [3, 4], [6, 3], [8, 1], [5, 4], [10, 0], [10, 0], [1, 6], [7, 2], [4, 1]];
+    const expected = 110;
+    const actual = game.scoreGame(frames);
+
+    expect(actual).toBe(expected);
+})
+
+test('scores a spare in the 10th frames', () => {
+    const frames = [[10, 0], [3, 4], [6, 3], [8, 1], [5, 4], [10, 0], [10, 0], [1, 6], [7, 2], [6, 4, 5]];
+
+    const expected = 15;
+    const actual = game.sumLastFrame(frames);
+
+    expect(actual).toBe(expected);
+}) 
