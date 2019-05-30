9ada816a20d9fe9a9cadeca928152e21c4d1211e
john-w-aupouri <49456910+john-w-aupouri@users.noreply.github.com>
2019-05-30 15:58:24 +1200

    Scoring a spare frame

--- game.js
+++ game.js
@@ -2,6 +2,11 @@ module.exports = {
   scoreFrame: scoreFrame
 }
 
-function scoreFrame (frame) {
-  return frame[0] + frame[1]
+function scoreFrame (frame, nextBall) {
+  if (frame[0] +frame[1] !== 10){         
+    return frame[0] + frame[1]
+  } else {
+    return frame[0] + frame[1] + nextBall[0] 
+  }
 }
+
--- tests/index.test.js
+++ tests/index.test.js
@@ -17,3 +17,19 @@ test('scores a normal frame', () => {
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
+
+test('scores a spare frame', () => {
+  const frame = [6, 4]
+  const nextFrame = [5, 3]
+  const expected = 15
+  const actual = game.scoreFrame(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike frame', () => {
+  const frame = [6, 4]
+  const nextFrame = [5, 3]
+  const expected = 18
+  const actual = game.scoreFrame(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
