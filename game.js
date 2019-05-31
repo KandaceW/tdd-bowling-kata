4e6bb7205f0ba17824e12353fbc6f7088d7da396
Daniel Rhodes <danielrhodesnz@gmail.com>
2019-06-01 10:58:13 +1200

    Checkpoint

--- game.js
+++ game.js
@@ -1,17 +1,8 @@
-module. exports = {
-  scoreFrame: scoreFrame,
+module.exports = {
+  scoreFrame: scoreFrame
 }
 
-
-function scoreFrame(frame, nextFrame) {
-  // if framescore = 10 then add the first item of the next frame.
-  // otherwise, framescore only equals the first and second item of the first frame
-  if(frameScore === 10 + nextFrame[0]) {
-  
-var frameScore = frame[0] + frame[1] + nextFrame[0]
-return frameScore
+function scoreFrame(frame){
+  return frame[0] + frame[1]
 }
-
-
-
 
