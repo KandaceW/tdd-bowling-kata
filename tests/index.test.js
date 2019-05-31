1bcdd4edb2126bc87953909963f42bb6196ae2e8
Daniel Rhodes <danielrhodesnz@gmail.com>
2019-06-01 10:47:20 +1200

    Commit/Checkpoint

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,29 +1,3 @@
-const game = require('../game') // this is the line to adspare
-
-// test('test setup working', () => {
-//   expect(true).toBeTruthy()
-// })
-
-test('scores a gutterball frame', () => {
-  const frame = [0, 0]
-  const expected = 0
-  const actual = game.scoreFrame(frame)
-  expect(actual).toBe(expected)
-})
-
-
-
-test('scores a normal frame', () => {
-  const frame = [2, 3]
-  const expected = 5
-  const actual = game.scoreFrame(frame)
-  expect(actual).toBe(expected)
-})
-
-test('scores a spare frame', () => {
-  const frame = [3 , 7]
-  const nextFrame = [2 , 0]
-  const expected = 12
-  const actual = game.scoreFrame(frame, nextFrame)
-  expect(actual).toBe(expected)
+test('test setup working', () => {
+  expect(true).toBeTruthy()
 })
