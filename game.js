af2e3cb08a5cf31dc1555b869269a9caefab1bad
Daniel Rhodes <danielrhodesnz@gmail.com>
2019-06-01 12:56:27 +1200

    Checkpoint

--- game.js
+++ game.js
@@ -1,8 +1,59 @@
 module.exports = {
-  scoreFrame: scoreFrame
+  scoreFrame: scoreFrame,
+  scoreSpare: scoreSpare,
+  scoreStrike: scoreStrike,
+  scoreGame: scoreGame,
+  sumLastFrame: sumLastFrame
 }
 
 function scoreFrame(frame){
   return frame[0] + frame[1]
 }
+
+function scoreSpare(frame, nextFrame){
+  var result = 0
+  if(scoreFrame(frame) === 10){
+    result += 10 + nextFrame[0]
+  }
+  return result
+}
+
+function scoreStrike(frame, nextFrame, thirdFrame){
+  var result = 0
+  if(frame[0] === 10 && nextFrame[0] === 10){
+    result = result + scoreFrame(frame) + scoreFrame(nextFrame) + thirdFrame[0]
+  } else if(frame[0] === 10){  
+    result = result + scoreFrame(frame) + scoreFrame(nextFrame)
+  }
+  return result
+}
+
+function scoreGame(frame){
+  var result = 0
+  for(var i = 0; i < frame.length; i++){
+    var firstFrame = frame[i]
+    var secondFrame = frame[i + 1]
+    var thirdFrame = frame[i + 2]
+    if (firstFrame[0] === 10 && i < 9){
+      result = result + scoreStrike(firstFrame, secondFrame, thirdFrame)
+    } else if (firstFrame + secondFrame === 10 && i < 9){
+      result = result + scoreSpare(firstFrame, secondFrame)
+    } else {
+      result = result + scoreFrame(firstFrame)
+    }
+  }
+  return result
+}
+
+function sumLastFrame(frame){
+  var result = 0
+  var lastFrame = frame[frame.length - 1]
+  result = result + lastFrame[0] + lastFrame[1] + lastFrame[2]
+  console.log(result);
+  return result
+}
+
+
+
+
 
--- tests/index.test.js
+++ tests/index.test.js
@@ -11,4 +11,48 @@ test('scores a gutterball frame', () => {
   expect(actual).toBe(expected)
 })
 
-test
+test('scores a normal fram', () => {
+  const frame = [2, 3]
+  const expected = 5
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a spare frame', () => {
+  const frame = [1, 9]
+  const nextFrame = [4, 2]
+  const expected = 14
+  const actual = game.scoreSpare(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike frame', () => {
+  const frame = [10, 0]
+  const nextFrame = [4, 3]
+  const expected = 17
+  const actual = game.scoreStrike(frame, nextFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a double strike frame', () => {
+  const frame = [10, 0]
+  const nextFrame = [10, 0]
+  const thirdFrame = [1, 3]
+  const expected = 21
+  const actual = game.scoreStrike(frame, nextFrame, thirdFrame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a game', () => {
+  const frame = [[10, 0], [3, 4], [6, 3], [8, 1], [5, 4], [10, 0], [10, 0], [1, 6], [7, 2], [4, 1]]
+  const expected = 110
+  const actual = game.scoreGame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a spare in the 10th frame', () => {
+  const frame = [[10, 0], [3, 4], [6, 3], [8, 1], [5, 4], [10, 0], [10, 0], [1, 6], [7, 2], [6, 4, 5]]
+  const expected = 15
+  const actual = game.sumLastFrame(frame)
+  expect(actual).toBe(expected)
+})
